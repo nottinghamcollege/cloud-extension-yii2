8a9c28621864e6404c86d2b791c281ed9d7a6516
Tim Kelty <tim@craftcms.com>
2023-02-23 16:43:52 -0500

    Mutex config

--- src/Module.php
+++ src/Module.php
@@ -6,38 +6,53 @@ use craft\helpers\App;
 
 class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
 {
+    public const REDIS_DATABASE_CACHE = 0;
+    public const REDIS_DATABASE_SESSION = 1;
+    public const REDIS_DATABASE_MUTEX = 2;
+    public const MUTEX_EXPIRE_WEB = 30;
+    public const MUTEX_EXPIRE_CONSOLE = 900;
+
     /**
      * @inheritDoc
      */
     public function bootstrap($app): void
     {
+        if (!Craft::$app->getRequest()->getIsConsoleRequest()) {
+            $app->setComponents([
+                'session' => [
+                    'class' => \yii\redis\Session::class,
+                    'redis' => self::getRedisConfig() + [
+                        'database' => self::REDIS_DATABASE_SESSION
+                    ],
+                ] + App::sessionConfig(),
+            ]);
+        }
+
         $app->setComponents([
             'cache' => [
                 'class' => \yii\redis\Cache::class,
-                'redis' => self::getRedisConfig(),
+                'redis' => self::getRedisConfig() + [
+                    'database' => self::REDIS_DATABASE_CACHE
+                ],
                 'defaultDuration' => Craft::$app->getConfig()->getGeneral()->cacheDuration,
-                // 'keyPrefix' => 'cache/',
-                // 'shareDatabase' => false,
             ],
-            'session' => [
-                'class' => \yii\redis\Session::class,
-                'redis' => self::getRedisConfig(),
-                // 'keyPrefix' => 'session/',
-            ] + App::sessionConfig(),
             'mutex' => [
                 'class' => \craft\mutex\Mutex::class,
                 'mutex' => [
                     'class' => \craft\cloud\Mutex::class,
-                    'redis' => self::getRedisConfig(),
-                    // 'keyPrefix' => 'mutex/'
-                    // 'expire' => Craft::$app->request->isConsoleRequest ? 900 : 30,
+                    'redis' => self::getRedisConfig() + [
+                        'database' => self::REDIS_DATABASE_MUTEX
+                    ],
+                    'expire' => Craft::$app->getRequest()->getIsConsoleRequest()
+                        ? self::MUTEX_EXPIRE_CONSOLE
+                        : self::MUTEX_EXPIRE_WEB,
                 ],
             ],
             'queue' => [
                 'class' => \craft\queue\Queue::class,
                 'proxyQueue' => [
                     'class' => \yii\queue\sqs\Queue::class,
-                    'url' => '',
+                    'url' => App::env('CRAFT_CLOUD_SQS_URL'),
                 ],
             ],
         ]);
