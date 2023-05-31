ee68143e4af9c6872d9af6aa8cec62f82f2783be
Tim Kelty <tim@craftcms.com>
2023-05-31 14:52:29 -0400

    Deps

--- src/AssetBehavior.php
+++ src/AssetBehavior.php
@@ -2,7 +2,54 @@
 
 namespace craft\cloud;
 
-class AssetBehavior
-{
+use Craft;
+use craft\base\Fs;
+use craft\elements\Asset;
+use craft\errors\FileException;
+use craft\errors\VolumeException;
+use craft\events\AssetEvent;
+use craft\helpers\Assets;
+use craft\helpers\FileHelper;
+use craft\helpers\Image;
+use DateTime;
+use yii\base\Behavior;
 
+/**
+ * @property $owner Asset
+ */
+class AssetBehavior extends Behavior
+{
+    public string $tempBucket;
+    public string $tempBucketKey;
+
+    public function events(): array
+    {
+        return parent::events() + [
+            // Asset::EVENT_BEFORE_HANDLE_FILE => 'beforeHandleFile'
+        ];
+    }
+
+    public function beforeHandleFile(AssetEvent $event): void
+    {
+        /** @var Asset|AssetBehavior $asset */
+        $asset = $event->asset;
+        $newLocation = $asset->newLocation;
+        $asset->newLocation = null;
+        $asset->tempFilePath = null;
+
+        if ($asset->getScenario() !== Asset::SCENARIO_CREATE) {
+            return;
+        }
+
+        if (!isset($asset->tempBucket, $asset->tempBucketKey, $newLocation)) {
+            return;
+        }
+
+        [$folderId, $filename] = Assets::parseFileLocation($newLocation);
+
+        $newFolder = Craft::$app->getAssets()->getFolderById($folderId);
+        $newVolume = $newFolder->getVolume();
+
+        Craft::$app->getAssets()->moveAsset($asset, $newFolder, $filename);
+    }
 }
--- src/controllers/CloudController.php
+++ src/controllers/CloudController.php
@@ -1,69 +1,43 @@
 <?php
 
-namespace craft\cloud\console\controllers;
+namespace craft\cloud\controllers;
 
 use Craft;
-use craft\cloud\AssetHelper;
-use craft\cloud\AssetManager;
-use craft\console\Controller;
-use craft\helpers\Console;
-use Illuminate\Support\Collection;
-use Symfony\Component\Process\Process;
-use Throwable;
-use yii\console\ExitCode;
-use yii\web\AssetBundle;
+use craft\cloud\fs\StorageFs;
+use craft\web\Controller;
+use yii\web\Response;
 
 class CloudController extends Controller
 {
-    protected array $processes = [];
-    public function actionPublishAssetBundle(string $className): int
+    public function actionGetUploadUrl(): Response
     {
-        /** @var AssetBundle $assetBundle */
-        $assetBundle = Craft::createObject($className);
+        $this->requireAcceptsJson();
+        $this->requirePostRequest();
+        $file = $this->request->getRequiredBodyParam('file');
+        $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $filename = sprintf('%s.%s', uniqid('upload', true), $extension);
 
-        if (!is_subclass_of($className, AssetBundle::class)) {
-            $this->stderr(sprintf('“%s” must be a subclass of “%s”.', $className, AssetBundle::class), Console::FG_RED);
-            return ExitCode::USAGE;
-        }
+        /** @var StorageFs $tmpFs */
+        $fs = Craft::createObject([
+            'class' => StorageFs::class,
+            'subfolder' => 'tmp',
+        ]);
 
-        $assetManager = Craft::$app->getAssetManager();
-        $assetBundle->publish($assetManager);
+        $cmd = $fs->getClient()->getCommand('PutObject', [
+            'Bucket' => $fs->getBucketName(),
+            'Key' => $fs->prefixPath($filename),
+        ]);
 
-        return ExitCode::OK;
-    }
+        // TODO: use setting
+        $s3Request = $fs->getClient()->createPresignedRequest($cmd, '+20 minutes');
+        $url = (string) $s3Request->getUri();
 
-    public function actionPublishAssetBundles(): int
-    {
-        $classMap = require(Craft::getAlias('@vendor/composer/autoload_classmap.php'));
-
-        Collection::make($classMap)
-            ->keys()
-            ->filter(fn($className) => str_contains($className, '\\assets\\') || preg_match('/Asset(Bundle)?$/', $className))
-            ->each(function(string $className) {
-                $process = new Process([
-                    PHP_BINARY,
-                    $this->request->getScriptFile(),
-                    'cloud/publish-asset-bundle',
-                    $className,
-                ]);
-
-                // TODO: run in parallel
-                try {
-                    $this->do("Publishing “{$className}”", fn() => $process->run(function ($type, $buffer) {
-                        if ($type === Process::ERR) {
-                            return;
-                        }
-
-                        $this->stdout($buffer);
-                    }));
-                } catch(Throwable) {
-                    // Carry on, we expect some failures
-                }
-
-                return $process;
-            });
-
-        return ExitCode::OK;
+        return $this->asJson([
+            'url' => $url,
+            'file' => $file,
+            'bucket' => $fs->getBucketName(),
+            'key' => $fs->prefixPath($filename),
+        ]);
     }
 }
 
