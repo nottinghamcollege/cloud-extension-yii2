bc0137cacebe499325033dd2e672eebb77332bc1
Tim Kelty <tim@craftcms.com>
2023-06-02 04:16:13 -0400

    uploader js

--- src/controllers/CloudController.php
+++ src/controllers/CloudController.php
@@ -8,6 +8,7 @@ use craft\cloud\fs\StorageFs;
 use craft\elements\Asset;
 use craft\helpers\Assets;
 use craft\web\Controller;
+use DateTime;
 use yii\web\Response;
 
 class CloudController extends Controller
@@ -16,10 +17,9 @@ class CloudController extends Controller
     {
         $this->requireAcceptsJson();
         $this->requirePostRequest();
-        $file = $this->request->getRequiredBodyParam('file');
-        $filename = $file['name'];
-        // $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
-        // $filename = sprintf('%s.%s', uniqid('upload', true), $extension);
+        $originalFilename = $this->request->getRequiredBodyParam('filename');
+        $extension = pathinfo($originalFilename, PATHINFO_EXTENSION);
+        $filename = sprintf('%s.%s', uniqid('upload', true), $extension);
         $folderId = $this->request->getRequiredBodyParam('folderId');
         $folder = Craft::$app->getAssets()->findFolder(['id' => $folderId]);
         $pathInVolume = "{$folder->path}$filename";
@@ -27,6 +27,7 @@ class CloudController extends Controller
         /** @var StorageFs $fs */
         $fs = $folder->getVolume()->getFs();
 
+        // TODO: add an fs upload and use that
         $cmd = $fs->getClient()->getCommand('PutObject', [
             'Bucket' => $fs->getBucketName(),
             'Key' => $fs->prefixPath($pathInVolume),
@@ -38,10 +39,10 @@ class CloudController extends Controller
 
         return $this->asJson([
             'url' => $url,
-            'file' => $file,
+            'originalFilename' => $originalFilename,
+            'filename' => $filename,
             'bucket' => $fs->getBucketName(),
             'key' => $fs->prefixPath($filename),
-            'filename' => $filename,
         ]);
     }
 
@@ -50,34 +51,132 @@ class CloudController extends Controller
     {
         $this->requireAcceptsJson();
 
+        $filename = $this->request->getRequiredBodyParam('filename');
+        $originalFilename = $this->request->getRequiredBodyParam('originalFilename');
+        $lastModified = $this->request->getBodyParam('lastModified');
         $elementsService = Craft::$app->getElements();
-        $tempBucket = $this->request->getBodyParam('bucket') ?: null;
-        $tempBucketKey = $this->request->getBodyParam('key') ?: null;
-        $filename = $this->request->getBodyParam('filename') ?: null;
+
+        if (!$filename) {
+            throw new BadRequestHttpException('No file was uploaded');
+        }
+
         $folderId = (int)$this->request->getBodyParam('folderId') ?: null;
         $fieldId = (int)$this->request->getBodyParam('fieldId') ?: null;
 
-        $folder = Craft::$app->getAssets()->findFolder(['id' => $folderId]);
+        if (!$folderId && !$fieldId) {
+            throw new BadRequestHttpException('No target destination provided for uploading');
+        }
 
-        $targetFs = $folder->getVolume()->getFs();
-        $targetPath = $folder->path . $filename;
+        $assets = Craft::$app->getAssets();
 
-        /** @var Fs $fs */
-        // $fs = Craft::createObject(Fs::class);
-        // $fs->renameFile($tempBucketKey, $targetPath);
+        if (empty($folderId)) {
+            /** @var AssetsField|null $field */
+            $field = Craft::$app->getFields()->getFieldById((int)$fieldId);
+
+            if (!$field instanceof AssetsField) {
+                throw new BadRequestHttpException('The field provided is not an Assets field');
+            }
+
+            if ($elementId = $this->request->getBodyParam('elementId')) {
+                $siteId = $this->request->getBodyParam('siteId') ?: null;
+                $element = $elementsService->getElementById($elementId, null, $siteId);
+            } else {
+                $element = null;
+            }
+            $folderId = $field->resolveDynamicPathToFolderId($element);
+
+            $selectionCondition = $field->getSelectionCondition();
+            if ($selectionCondition instanceof ElementCondition) {
+                $selectionCondition->referenceElement = $element;
+            }
+        } else {
+            $selectionCondition = null;
+        }
+
+        if (empty($folderId)) {
+            throw new BadRequestHttpException('The target destination provided for uploading is not valid');
+        }
+
+        $folder = $assets->findFolder(['id' => $folderId]);
+
+        if (!$folder) {
+            throw new BadRequestHttpException('The target folder provided for uploading is not valid');
+        }
+
+        // Check the permissions to upload in the resolved folder.
+        // $this->requireVolumePermissionByFolder('saveAssets', $folder);
+
+        // $normalizedFilename = Assets::prepareAssetName($filename);
+
+        // if ($selectionCondition) {
+        //     $tempFolder = Craft::$app->getAssets()->getUserTemporaryUploadFolder();
+        //     if ($folder->id !== $tempFolder->id) {
+        //         // upload to the user's temp folder initially, with a temp name
+        //         $originalFolder = $folder;
+        //         $originalFilename = $filename;
+        //         $folder = $tempFolder;
+        //         $filename = uniqid('asset', true) . '.' . pathinfo($filename, PATHINFO_EXTENSION);
+        //     }
+        // }
 
         $asset = new Asset();
+        $asset->newFilename = Assets::prepareAssetName($originalFilename);
         $asset->setFilename($filename);
         $asset->folderId = $folder->id;
         $asset->setVolumeId($folder->volumeId);
         $asset->uploaderId = Craft::$app->getUser()->getId();
         $asset->avoidFilenameConflicts = true;
-        $asset->dateModified = new \DateTime();
+        $asset->dateModified = $lastModified ? new DateTime('@' . $lastModified) : null;
+
+        // kind, size, width, height?
+
+        if (isset($originalFilename)) {
+            $asset->title = Assets::filename2Title(pathinfo($originalFilename, PATHINFO_FILENAME));
+        }
+
         $result = $elementsService->saveElement($asset);
 
+        // In case of error, let user know about it.
         if (!$result) {
             $errors = $asset->getFirstErrors();
             return $this->asFailure(implode("\n", $errors));
+        }
+
+        // if ($selectionCondition) {
+        //     if (!$selectionCondition->matchElement($asset)) {
+        //         // delete and reject it
+        //         $elementsService->deleteElement($asset, true);
+        //         return $this->asFailure(Craft::t('app', '{filename} isn’t selectable for this field.', [
+        //             'filename' => $uploadedFile->name,
+        //         ]));
+        //     }
+        //
+        //     if (isset($originalFilename, $originalFolder)) {
+        //         // move it into the original target destination
+        //         $asset->newFilename = $originalFilename;
+        //         $asset->newFolderId = $originalFolder->id;
+        //         $asset->setScenario(Asset::SCENARIO_MOVE);
+        //
+        //         if (!$elementsService->saveElement($asset)) {
+        //             $errors = $asset->getFirstErrors();
+        //             return $this->asJson([
+        //                 'error' => $this->asFailure(implode("\n", $errors)),
+        //             ]);
+        //         }
+        //     }
+        // }
+
+        if ($asset->conflictingFilename !== null) {
+            $conflictingAsset = Asset::findOne(['folderId' => $folder->id, 'filename' => $asset->conflictingFilename]);
+
+            return $this->asJson([
+                'conflict' => Craft::t('app', 'A file with the name “{filename}” already exists.', ['filename' => $asset->conflictingFilename]),
+                'assetId' => $asset->id,
+                'filename' => $asset->conflictingFilename,
+                'conflictingAssetId' => $conflictingAsset->id ?? null,
+                'suggestedFilename' => $asset->suggestedFilename,
+                'conflictingAssetUrl' => ($conflictingAsset && $conflictingAsset->getVolume()->getFs()->hasUrls) ? $conflictingAsset->getUrl() : null,
+            ]);
         }
 
         return $this->asSuccess(data: [
--- src/web/assets/uploader/src/uploader.js
+++ src/web/assets/uploader/src/uploader.js
@@ -1,42 +1,62 @@
 /** global: Craft */
 /** global: Garnish */
+
+const getUploadUrl = function(data) {
+  const formData = data.formData;
+  const file = data.files[0];
+
+  return Craft.sendActionRequest('POST', 'cloud/get-upload-url', {
+    data: Object.assign(formData, {
+      file: {
+        lastModified: file.lastModified,
+        name: file.name,
+        size: file.size,
+        type: file.type,
+      },
+    }),
+  }).then(() => data.submit());
+}
+
 Craft.CloudUploader = Craft.Uploader.extend({
   init: function ($element, settings) {
     this.base($element, settings);
     this.uploader.on('fileuploadsubmit', this.onSubmit.bind(this));
+    this.uploader.on('fileuploaddone', this.onDone.bind(this));
   },
-  onSubmit: function (e, data) {
-    const file = data.files[0];
+  onDone: function(event, data) {
+    data.jqXHR = axios.post(
+      Craft.getActionUrl('cloud/create-asset'),
+      data.formData,
+    );
+  },
+  onSubmit: function(event, data) {
     const formData = this.uploader.fileupload('option', 'formData');
+    const file = data.files[0];
+    const filename = file.name;
+    Object.assign(formData, {filename});
 
     data.jqXHR = Craft.sendActionRequest('POST', 'cloud/get-upload-url', {
-      data: {
-        file: {
-          lastModified: file.lastModified,
-          name: file.name,
-          size: file.size,
-          type: file.type,
-        },
-      },
-    })
-      .then((response) => {
-        Object.assign(formData, response.data);
-        Object.assign(data, {
-          type: 'PUT',
-          url: response.data.url,
-        });
+      data: formData
+    }).then((response) => {
+      Object.assign(formData, response.data);
 
-        return this.uploader.fileupload('send', data);
-      })
-      .then(() => {
-        return $.ajax({
-          url: Craft.getActionUrl('cloud/create-asset'),
-          type: 'POST',
-          headers: this.uploader.fileupload('option', 'headers'),
-          data: formData,
-        });
+      return this.uploader.fileupload('send', {
+        url: response.data.url,
+        type: 'PUT',
+        files: [file],
       });
 
+      // Upload works but doesn't trigger stuff…
+      // return axios.put(response.data.url, file, {
+      //   headers: {
+      //     'Content-Type': file.type,
+      //   }
+      // }).then(() => axios.post(
+      //   Craft.getActionUrl('cloud/create-asset'),
+      //   formData,
+      // ));
+    });
+
     return false;
-  },
+  }
 });
