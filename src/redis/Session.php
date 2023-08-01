7126645be316e7ebc4f0b973eb276e8d0c5bd5d9
Tim Kelty <tim@craftcms.com>
2023-08-01 15:53:31 -0400

    Single db

--- src/redis/Session.php
+++ src/redis/Session.php
@@ -9,10 +9,7 @@ class Session extends \yii\redis\Session
     public function __construct($config = [])
     {
         $config += [
-            'redis' => [
-                'class' => Connection::class,
-                'database' => 1,
-            ],
+            'redis' => Connection::class,
             'keyPrefix' => sprintf('session.%s.', Module::getInstance()->getConfig()->environmentId),
         ];
 
