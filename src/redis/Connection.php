acdd0e684c73963a36bed200caa7c5a46ab64374
Tim Kelty <tim@craftcms.com>
2023-08-02 12:26:25 -0400

    Remove select

--- src/redis/Connection.php
+++ src/redis/Connection.php
@@ -31,15 +31,4 @@ class Connection extends \yii\redis\Connection
 
         parent::open();
     }
-
-    /**
-     * Ensure database is always set, even if connection is already open
-     */
-    public function executeCommand($name, $params = []): bool|array|string|null
-    {
-        if ($name !== 'SELECT') {
-            $this->executeCommand('SELECT', [$this->database]);
-        }
-        return parent::executeCommand($name, $params);
-    }
 }
