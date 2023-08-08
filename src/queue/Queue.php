016166db273cb7d7e47153969f2a9a3ab9802005
Tim Kelty <tim@craftcms.com>
2023-08-08 14:25:39 -0400

    Queue logging

--- src/queue/Queue.php
+++ src/queue/Queue.php
@@ -2,7 +2,10 @@
 
 namespace craft\cloud\queue;
 
+use Craft;
 use craft\cloud\Module;
+use samdark\log\PsrMessage;
+use yii\base\NotSupportedException;
 
 class Queue extends \yii\queue\sqs\Queue
 {
@@ -15,5 +18,56 @@ class Queue extends \yii\queue\sqs\Queue
         ];
 
         parent::__construct($config);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function pushMessage($message, $ttr, $delay, $priority)
+    {
+        if ($priority) {
+            throw new NotSupportedException('Priority is not supported in this driver');
+        }
+
+        $request = [
+            'QueueUrl' => $this->url,
+            'MessageBody' => $message,
+            'DelaySeconds' => $delay,
+            'MessageAttributes' => [
+                'TTR' => [
+                    'DataType' => 'Number',
+                    'StringValue' => $ttr,
+                ],
+            ],
+        ];
+
+        if (substr($this->url, -5) === '.fifo') {
+            $request['MessageGroupId'] = $this->messageGroupId;
+            $request['MessageDeduplicationId'] = hash('sha256', $message);
+        }
+
+        Craft::info(new PsrMessage(
+            'SQS request',
+            $request,
+        ));
+
+        try {
+            $response = $this->getClient()->sendMessage($request);
+        } catch(\Throwable $e) {
+            Craft::info(new PsrMessage(
+                'SQS exception',
+                [
+                    'message' => $e->getMessage(),
+                    'trace' => $e->getTrace(),
+                ],
+            ));
+        }
+
+        Craft::info(new PsrMessage(
+            'SQS response',
+            $response->toArray(),
+        ));
+
+        return $response['MessageId'];
     }
 }
