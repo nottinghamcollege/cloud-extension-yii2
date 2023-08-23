5b24bbdd3130b6a302d73b2f403d444a23bbc1c7
Tim Kelty <tim@craftcms.com>
2023-08-23 16:03:52 -0400

    187 on an undercover cop

--- src/runtime/event/CliHandler.php
+++ src/runtime/event/CliHandler.php
@@ -5,10 +5,13 @@ namespace craft\cloud\runtime\event;
 use Bref\Context\Context;
 use Bref\Event\Handler;
 use Exception;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
 use Symfony\Component\Process\Process;
 
 class CliHandler implements Handler
 {
+    public const EXIT_CODE_TIMEOUT = 187;
     protected string $scriptPath = '/var/task/craft';
 
     public function handle(mixed $event, Context $context): array
@@ -22,17 +25,21 @@ class CliHandler implements Handler
         $php = PHP_BINARY;
         $command = escapeshellcmd("{$php} {$this->scriptPath} {$commandArgs}");
         $timeout = max(1, $context->getRemainingTimeInMillis() / 1000 - 1);
-
         $process = Process::fromShellCommandline($command, null, [
             'LAMBDA_INVOCATION_CONTEXT' => json_encode($context, JSON_THROW_ON_ERROR),
         ], null, $timeout);
 
-        $process->mustRun(function($type, $buffer): void {
-            echo $buffer;
-        });
+        try {
+            /** @throws ProcessTimedOutException|ProcessFailedException */
+            $process->mustRun(function($type, $buffer): void {
+                echo $buffer;
+            });
+        } catch (ProcessTimedOutException $e) {
+            $exitCode = self::EXIT_CODE_TIMEOUT;
+        }
 
         return [
-            'exitCode' => $process->getExitCode(),
+            'exitCode' => $exitCode ?? $process->getExitCode(),
             'output' => $process->getOutput(),
         ];
     }
