b70d79701d2000dd9af5601ad02b627d986d8893
Tim Kelty <tim@craftcms.com>
2023-08-25 14:32:14 -0400

    Process pool

---
+++ src/AssetBundlePublisher.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace craft\cloud;
+
+use Craft;
+use craft\helpers\Console;
+use Illuminate\Support\Collection;
+use Iterator;
+use Symfony\Component\Process\Exception\RuntimeException;
+use Symfony\Component\Process\Process;
+use yii\base\BaseObject;
+
+class AssetBundlePublisher extends BaseObject
+{
+    public int $concurrency = 5;
+
+    /** @var array<string> */
+    public array $classNames;
+
+    /** @var Iterator<Process> */
+    protected Iterator $processes;
+
+    /** @var array<Process> */
+    protected array $runningProcesses = [];
+
+    public function __construct($config = [])
+    {
+        $config += [
+            'classNames' => $this->getAssetBundleClasses(),
+        ];
+
+        parent::__construct($config);
+    }
+
+    public function init(): void
+    {
+        $this->processes = $this->getProcesses();
+    }
+
+    public function wait(): void
+    {
+        $this->startNext();
+
+        while (count($this->runningProcesses) > 0) {
+            foreach ($this->runningProcesses as $key => $process) {
+                try {
+                    $process->checkTimeout();
+                    $isRunning = $process->isRunning();
+                } catch (RuntimeException $e) {
+                    $isRunning = false;
+                }
+
+                if (!$isRunning) {
+                    Console::stdout($process->getErrorOutput());
+                    Console::stdout($process->getOutput());
+                    unset($this->runningProcesses[$key]);
+                    $this->startNext();
+                }
+            }
+            usleep(1000);
+        }
+    }
+
+    protected function startNext(): void
+    {
+        while (count($this->runningProcesses) < $this->concurrency && $this->processes->valid()) {
+            $process = $this->processes->current();
+            $process->start();
+            $this->runningProcesses[] = $process;
+            $this->processes->next();
+        }
+    }
+
+    protected function getProcesses(): \Generator
+    {
+        $processes = Collection::make($this->classNames)
+            ->map(fn(string $className) => new Process([
+                PHP_BINARY,
+                Craft::$app->getRequest()->getScriptFile(),
+                'cloud/asset-bundles/publish-bundle',
+                $className,
+                '2>&1',
+            ])
+        );
+
+        foreach ($processes->all() as $process) {
+            yield $process;
+        }
+    }
+
+    protected function getAssetBundleClasses(): array
+    {
+        $classMap = require(Craft::getAlias('@vendor/composer/autoload_classmap.php'));
+
+        return Collection::make($classMap)
+            ->keys()
+            ->filter(function ($className): bool {
+                if (
+                    preg_match('/^craft\\\(elements|fieldlayoutelements|gql|records)/', $className) ||
+                    preg_match('/^yii\\\web\\\AssetBundle/', $className)
+                ) {
+                    return false;
+                }
+
+                // TODO: event
+                return (bool) preg_match('/(\\\assets\\\|assetbundles?|Asset(Bundle)?$)/', $className);
+            })
+            ->values()
+            ->all();
+    }
+}
--- src/console/controllers/AssetBundlesController.php
+++ src/console/controllers/AssetBundlesController.php
@@ -3,11 +3,10 @@
 namespace craft\cloud\console\controllers;
 
 use Craft;
+use craft\cloud\AssetBundlePublisher;
+use craft\cloud\queue\PublishAssetBundleJob;
 use craft\console\Controller;
-use craft\helpers\Console;
-use Illuminate\Support\Collection;
-use Symfony\Component\Process\Process;
-use Throwable;
+use ReflectionClass;
 use yii\console\Exception;
 use yii\console\ExitCode;
 use yii\web\AssetBundle;
@@ -16,75 +15,26 @@ class AssetBundlesController extends Controller
 {
     public function actionPublishBundle(string $className): int
     {
-        /** @var AssetBundle $assetBundle */
-        try {
-            $assetBundle = Craft::createObject($className);
+        $this->do("Publishing “{$className}”", function() use ($className) {
+            $rc = new ReflectionClass($className);
 
-            if (!is_subclass_of($className, AssetBundle::class)) {
-                throw new Exception();
+            if (!$rc->isSubclassOf(AssetBundle::class) || !$rc->isInstantiable()) {
+                throw new Exception('Not a valid asset bundle.');
             }
-        } catch(Throwable $e) {
-            $this->stderr(sprintf(
-                'Skipping “%s”',
-                $className,
-                AssetBundle::class,
-            ), Console::FG_RED);
-            $this->stderr(PHP_EOL);
 
-            return ExitCode::USAGE;
-        }
-
-        if (!is_subclass_of($className, AssetBundle::class)) {
-            $this->stderr(sprintf('“%s” must be a subclass of “%s”.', $className, AssetBundle::class), Console::FG_RED);
-            return ExitCode::USAGE;
-        }
-
-        $assetManager = Craft::$app->getAssetManager();
-        $assetBundle->publish($assetManager);
-
-        $this->stdout("Published “{$className}”");
-        $this->stdout(PHP_EOL);
+            /** @var AssetBundle $assetBundle */
+            $assetBundle = Craft::createObject($className);
+            $assetManager = Craft::$app->getAssetManager();
+            $assetBundle->publish($assetManager);
+        });
 
         return ExitCode::OK;
     }
 
     public function actionPublish(): int
     {
-        $classMap = require(Craft::getAlias('@vendor/composer/autoload_classmap.php'));
-
-
-        $this->stdout('Publishing asset bundles ...');
-        $this->stdout(PHP_EOL);
-
-        // TODO: run in parallel
-        Collection::make($classMap)
-            ->keys()
-            ->filter(function ($className): bool|int {
-                // TODO: event
-                return preg_match('/(\\\assets\\\|assetbundles?|Asset(Bundle)?$)/', $className);
-            })
-            ->mapWithKeys(function(string $className) {
-                $process = new Process([
-                    PHP_BINARY,
-                    $this->request->getScriptFile(),
-                    'cloud/asset-bundles/publish-bundle',
-                    $className,
-                    '2>&1',
-                ]);
-
-                $process->run();
-
-                $output = $process->isSuccessful()
-                    ? $process->getOutput()
-                    : $process->getErrorOutput();
-
-                $this->stdout("    - $output");
-
-                return [$className => $process];
-            });
-
-        $this->stdout('Done publishing asset bundles.');
-        $this->stdout(PHP_EOL);
+        $publisher = new AssetBundlePublisher();
+        $publisher->wait();
 
         return ExitCode::OK;
     }
