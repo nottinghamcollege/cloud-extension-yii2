71b4c22ea5fac2c093f08a122f41cda09899825e
Tim Kelty <tim@craftcms.com>
2023-09-12 00:27:40 -0400

    Don't add db tables if not installed

--- src/console/controllers/UpController.php
+++ src/console/controllers/UpController.php
@@ -2,6 +2,7 @@
 
 namespace craft\cloud\console\controllers;
 
+use Craft;
 use craft\console\Controller;
 use yii\console\ExitCode;
 
@@ -10,9 +11,12 @@ class UpController extends Controller
     public function actionIndex(): int
     {
         // TODO: wrap with events
-        $this->run('/setup/php-session-table');
-        $this->run('/setup/db-cache-table');
-        $this->run('/up');
+        if (Craft::$app->getIsInstalled()) {
+            $this->run('/setup/php-session-table');
+            $this->run('/setup/db-cache-table');
+            $this->run('/up');
+        }
+
         $this->run('asset-bundles/publish');
 
         return ExitCode::OK;
