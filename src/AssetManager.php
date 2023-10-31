73d081b53f15d8a6726f358b09f55c39edca9da3
Tim Kelty <tim@craftcms.com>
2023-10-31 14:44:15 -0400

    Use new cacheSourcePaths

--- src/AssetManager.php
+++ src/AssetManager.php
@@ -8,6 +8,8 @@ use craft\helpers\FileHelper;
 
 class AssetManager extends \craft\web\AssetManager
 {
+    public bool $cacheSourcePaths = false;
+
     public function init()
     {
         $this->preparePaths();
@@ -18,20 +20,6 @@ class AssetManager extends \craft\web\AssetManager
     {
         $this->preparePaths();
         return parent::publish($path, $options);
-    }
-
-    protected function hash($path): string
-    {
-        if (is_callable($this->hashCallback)) {
-            return call_user_func($this->hashCallback, $path);
-        }
-
-        $dir = is_file($path) ? dirname($path) : $path;
-
-        // TODO: what?
-        /** @phpstan-ignore-next-line */
-        $alias = Craft::alias($dir);
-        return sprintf('%x', crc32($alias . '|' . FileHelper::lastModifiedTime($path) . '|' . $this->linkAssets));
     }
 
     protected function preparePaths(): void
