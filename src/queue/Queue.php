09befea52a8ebb493e83ebcfa3bcb1493b8841ab
Tim Kelty <tim@craftcms.com>
2023-11-09 14:36:12 -0500

    Set default ttr

--- src/queue/Queue.php
+++ src/queue/Queue.php
@@ -11,6 +11,7 @@ class Queue extends \yii\queue\sqs\Queue
         $config += [
             'url' => Module::getInstance()->getConfig()->sqsUrl,
             'region' => Module::getInstance()->getConfig()->getRegion(),
+            'ttr' => 60 * 15 - 1,
         ];
 
         parent::__construct($config);
