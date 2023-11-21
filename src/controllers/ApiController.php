84af44fb946daa5bb374e65b2728ede53871a847
Tim Kelty <tim@craftcms.com>
2023-11-20 20:36:31 -0500

    API controller

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -3,12 +3,14 @@
 namespace craft\cloud\controllers;
 
 use craft\web\Controller;
+use craft\web\Response;
 
 class ApiController extends Controller
 {
     protected array|bool|int $allowAnonymous = true;
 
-    public function actionIndex()
+    public function actionIndex(): Response
     {
+        return $this->response;
     }
 }
